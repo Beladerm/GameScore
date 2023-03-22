bf23b0686bd1cafc1208e26b09cb1396ebe97ec0
Artem <90094274+Beladerm@users.noreply.github.com>
2023-03-23 00:42:56 +0300

    game score clicker final version

--- app/src/main/java/ru/ms/stu/gamescore/MainActivity.kt
+++ app/src/main/java/ru/ms/stu/gamescore/MainActivity.kt
@@ -2,10 +2,29 @@ package ru.ms.stu.gamescore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import ru.ms.stu.gamescore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var score1 = -1
+    private var score2 = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        changeScore()
+    }
+
+    private fun changeScore() {
+        with(binding) {
+            src1.setOnClickListener(View.OnClickListener {
+                src1.text = (++score1).toString()
+            })
+            src2.setOnClickListener(View.OnClickListener {
+                src2.text = (++score2).toString()
+            })
+        }
     }
 }
